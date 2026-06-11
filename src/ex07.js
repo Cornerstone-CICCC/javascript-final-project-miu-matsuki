b5f06d4d4b7a4ab48e5447d5b614f9e12dcdbcbe
miu-matsuki <miu.m.works@gmail.com>
2026-06-11 11:55:29 -0700

    ex07.js done

--- src/ex07.js
+++ src/ex07.js
@@ -4,6 +4,14 @@
  * @returns {{positive: number[], negative: number[], zero: number[]}}
  */
 function classifyNumbers(arr) {
+  if (!arr) throw new Error('Array cannot be undefined');
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
+  
+  //console.log(arr)
+
+  if ( arr.some((num)=> typeof num !== "number")) throw new Error('Array can only contain numbers');
+
+
   return {
     positive: arr.filter((number) => number > 0),
     negative: arr.filter((number) => number < 0),
