796362d85e378422550257478b5f6ce579c99324
miu-matsuki <miu.m.works@gmail.com>
2026-06-11 09:58:42 -0700

    ex01.js done

--- src/ex01.js
+++ src/ex01.js
@@ -4,9 +4,26 @@
  * @returns {number}
  */
 function findMax(arr) {
+  
+  if (!arr) throw new Error('Array cannot be undefined');
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
   if (arr.length <= 0) throw new Error('Array cannot be empty');
 
-  return arr[0];
+  console.log(arr)
+
+  //Set max
+  let max= arr[0];
+  arr.map((num)=>{
+    
+    if (typeof num !== "number") throw new Error('Array can only contain numbers');
+    if(num > max){
+      //   console.log(num>max)
+      max = num;
+    }
+})
+
+console.log(Number(max))
+return Number(max)
 }
 
 module.exports = findMax;
