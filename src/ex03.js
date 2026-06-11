a54891d7d542459495979de7412104fbbbc12fab
miu-matsuki <miu.m.works@gmail.com>
2026-06-11 11:05:47 -0700

    ex03.js done

--- src/ex03.js
+++ src/ex03.js
@@ -4,6 +4,13 @@
  * @returns {number}
  */
 function calculateFactorial(number) {
+  if (!number && number!== 0) throw new Error('Number cannot be undefined');
+  if(number < 0) throw new Error('Number cannot be negative');
+  //0.01, 1.24
+  if(number % 1 !== 0) throw new Error('Number must be an integer');
+  if (typeof number !== "number") throw new Error('Argument must be a number');
+
+
   let result = 1;
 
   for (let current = 1; current <= number; current += 1) {
