ddb36c5be4332683f413a551a4abbf58cb64e717
miu-matsuki <miu.m.works@gmail.com>
2026-06-11 11:20:51 -0700

    ex04.js done

--- src/ex04.js
+++ src/ex04.js
@@ -4,7 +4,18 @@
  * @returns {number}
  */
 function calculateAverage(arr) {
-  const total = arr.reduce((sum, number) => sum + number, 0);
+  if (!arr) throw new Error('Array cannot be undefined');
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
+  if (arr.length <= 0) throw new Error('Array cannot be empty');
+
+
+    console.log(arr)
+  const total = arr.reduce((sum, number) => {
+    console.log(sum)
+    if (typeof number !== "number") throw new Error('Array can only contain numbers');
+    return sum + number
+   // console.log(sum)
+  }, 0);
   return total / arr.length;
 }
 
