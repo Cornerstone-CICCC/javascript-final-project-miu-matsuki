4dd094fc633f3cd73c13d35d24160d6b8847e11c
miu-matsuki <miu.m.works@gmail.com>
2026-06-11 11:27:27 -0700

    ex05.js done

--- src/ex05.js
+++ src/ex05.js
@@ -4,7 +4,14 @@
  * @returns {number[]}
  */
 function filterEvenNumbers(arr) {
-  return arr.filter((number) => number % 2 === 0);
+
+  if (!arr) throw new Error('Array cannot be undefined');
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
+
+  return arr.filter((number) => {
+    if (typeof number !== "number") throw new Error('Array can only contain numbers');
+    return number % 2 === 0
+  });
 }
 
 module.exports = filterEvenNumbers;
