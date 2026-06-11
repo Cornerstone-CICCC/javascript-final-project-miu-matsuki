a3c496258c207e7e4e50dcb9de7936f2b7239365
miu-matsuki <miu.m.works@gmail.com>
2026-06-11 10:45:26 -0700

    ex02.js done

--- src/ex02.js
+++ src/ex02.js
@@ -4,7 +4,17 @@
  * @returns {number}
  */
 function sumArray(arr) {
-  return arr.reduce((total, number) => total + number);
+  
+  //console.log(arr)
+  if (!arr) throw new Error('Array cannot be undefined');
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
+ 
+  return arr.reduce((total, number) => {
+    console.log(total);
+    if (typeof number !== "number") throw new Error('Array can only contain numbers');
+    return Number(total + number)
+ }
+  ,0);
 }
 
 module.exports = sumArray;
