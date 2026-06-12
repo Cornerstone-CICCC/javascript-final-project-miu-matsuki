b6fe2962b4671468d5be965f2057d87428e010fe
miu-matsuki <miu.m.works@gmail.com>
2026-06-12 11:30:48 -0700

    ex12.js done!

--- src/ex12.js
+++ src/ex12.js
@@ -5,10 +5,32 @@
  * @returns {{characters: number, words: number, sentences: number}}
  */
 function analyzeText(str) {
+  
+  if(!str&& str!=="")throw new Error ('String cannot be undefined'); 
+
+  if(typeof str !== "string")throw new Error ('Argument must be a string');
+  
+  let outCharacters = 0;
+  let outWord = 0;
+  let outSentence = 0;
+
+  //returns zero values for an empty string
+  if(str !== ""){
+    
+    //returns zero values for a string containing only spaces
+    if(str.trim().split(/\s+/).join("") !== ""){
+      outWord = str.trim().split(/\s+/).length;
+      outSentence = str.replace("!", ".").replace("?", ".").split('.').length - 1;
+      //'does not require punctuation for one sentence'
+      if(outSentence === 0) {outSentence+=1}
+
+    }
+  }
+
   return {
     characters: str.length,
-    words: str.split(' ').length,
-    sentences: str.split('.').length - 1,
+    words:outWord,
+    sentences: outSentence
   };
 }
 
