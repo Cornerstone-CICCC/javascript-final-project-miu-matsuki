5656807194b472432a657fab7cfcba35ef2f8f9d
miu-matsuki <miu.m.works@gmail.com>
2026-06-12 12:17:27 -0700

    ex11.js done!

--- src/ex11.js
+++ src/ex11.js
@@ -6,8 +6,30 @@
  * @returns {Array}
  */
 function rotateArray(arr, positions) {
-  const splitIndex = arr.length - positions;
-  return [...arr.slice(splitIndex), ...arr.slice(0, splitIndex)];
+  //arr-----------------------------------------
+  if (!arr) throw new Error('Array cannot be undefined');
+  if (!Array.isArray(arr)) throw new Error('Argument must be an array');
+  // positions----------------------------------
+  if (!positions && positions !== 0) throw new Error('Positions cannot be undefined');
+  if (typeof positions !== "number") throw new Error('Positions must be a number');
+  if(positions %1 !== 0) throw new Error ('Positions must be an integer')
+
+  //--------------------------------------------
+  //positive
+ 
+  let splitIndex = arr.length - positions; //3
+  let output =  [...arr.slice(splitIndex), ...arr.slice(0, splitIndex)];
+
+  if(positions < 0){
+     splitIndex = positions * -1; //1
+     if(positions*-1 >  arr.length){
+      splitIndex = (positions + arr.length) * -1;
+     }
+
+     output =  [...arr.slice(splitIndex),...arr.slice(0, splitIndex)]; // 234   1
+  }
+
+  return output;
 }
 
 module.exports = rotateArray;
